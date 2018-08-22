066325c8e0317415aa236406a9aa9ea5e842d948
Nicole <npeery@gmail.com>
2018-08-22 07:34:25 -0700

    Added jQuery to project and alert button functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+// chart variables
 
+// Chart.defaults.global.responsive = true;
+// Chart.defaults.global.maintainAspectRatio = false;
+// var ctx1 = document.getElementById("traffic-chart").getContext('2d');
+// var ctx2 = document.getElementById("daily-traffic-chart").getContext('2d');
+// var ctx3 = document.getElementById("mobile-user-chart").getContext('2d');
+
+// alert button fade in and hide
+
+$('.alert').hide();
+$('.alert').fadeIn(1000);
+
+$('.x').click(function () {
+  $('.alert').hide();
+});
